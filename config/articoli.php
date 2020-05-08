784c18fc6530a6ffb1176006805ad43b0678bc3e
Giovanni <giovanni.diana511@gmail.com>
2020-05-08 19:38:40 +0200

    First push

---
+++ config/articoli.php
@@ -0,0 +1,41 @@
+<?php
+    $articoli = [
+        [
+            'avatar' => "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/b4b85f7e-254c-4f22-afdb-ab48caba6e0a/eric-olive-200px.jpg",
+            'nomeAutore' => "Mario",
+            'titolo' => "Prova",
+            'descrizione' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'data' => "07/05/2020"
+        ],
+        [
+            'avatar' => "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/e8ac629e-18d6-4c35-bf97-d6a0c3351560/david-atanda-200x200.jpg",
+            'nomeAutore' => "Gennaro",
+            'titolo' => "Prova",
+            'descrizione' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'data' => "07/05/2020"
+        ],
+        [
+            'avatar' => "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/d8b1ae77-d6d7-4ea4-b1c7-3610213b57ea/ben-frain-200x200.jpg",
+            'nomeAutore' => "Alfonso",
+            'titolo' => "Prova",
+            'descrizione' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'data' => "07/05/2020"
+        ],
+        [
+            'avatar' => "https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/c1dda88d-056e-4fdf-9827-76237a824556/vitaly-friedman.jpg",
+            'nomeAutore' => "Giuseppe",
+            'titolo' => "Prova",
+            'descrizione' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'data' => "07/05/2020"
+        ],
+        [
+            'avatar' => "",
+            'nomeAutore' => "Tommaso",
+            'titolo' => "Prova",
+            'descrizione' => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'data' => "07/05/2020"
+        ]
+    ];
+
+    return $articoli;
+?>
